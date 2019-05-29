f8a82de77860ef2d11abff3b463010887ac789a1
DUNCAN CADE <150041674@cart.ads>
2019-05-29 09:02:28 -0700

    Fixed detailed portfolio pages, and added new feature to them

--- script.js
+++ script.js
@@ -74,3 +74,98 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".text > .wrap { border-right: 0.08em solid #bbb}";
         document.body.appendChild(css);
     };
+/* Tic-Tac-Toe*/
+
+function move() {
+    window.location.replace("tictactoe.html")
+}
+
+var gameBoard;
+const playerH = 'O';
+const playerAI = 'X';
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+	document.getElementById("winner").style.display = "none";
+	gameBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick);
+	}
+}
+
+function turnClick(square) {
+	if (typeof gameBoard[square.target.id] == 'number') {
+		turn(square.target.id, playerH)
+		if (!checkTie()) turn(bestSpot(), playerAI);
+	}
+}
+
+function turn(squareId, player) {
+	gameBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWin(gameBoard, player)
+	if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) => 
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == playerH ? "blue" : "red";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick);
+	}
+	declareWinner(gameWon.player == playerH ? "You win!" : "You lose!");
+}
+
+function declareWinner(who) {
+	document.getElementById("winner").style.display = "block";
+	document.getElementById("text").innerText = who;
+}
+
+function emptySquares() {
+	return gameBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return emptySquares()[0];
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "grey";
+			cells[i].removeEventListener('click', turnClick);
+		}
+		declareWinner("Tie Game!")
+		return true;
+	}
+	return false;
+}
